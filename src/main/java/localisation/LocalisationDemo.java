c52fc35644945b6eb68f2b4cfc53ba73c5397fd6
gihanf <gihan.fernando@gmail.com>
2017-03-21 21:15:26 +1100

    Added localisation demo.

---
+++ src/main/java/localisation/LocalisationDemo.java
@@ -0,0 +1,35 @@
+package localisation;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class LocalisationDemo {
+    public static void main(String[] args) {
+
+        /**
+         * Locale must be created with either:
+         * - a lowercase language e.g. "en"
+         * - a lowercase language and an uppercase country code e.g. "en" "US"
+        */
+
+        /** Ways to create a Locale **/
+
+        // Locale with language and country code
+        Locale myLocale = new Locale("en", "AU");
+        System.out.println("myLocale = " + myLocale);       // "en_AU"
+
+        // Locale with builder
+        Locale l2 = new Locale.Builder()
+                .setRegion("IN")
+                .setLanguage("hi")
+                .build();
+
+        // Locale with language only
+        myLocale = new Locale("en");
+        myLocale = Locale.ENGLISH;
+
+        ResourceBundle bundle = ResourceBundle.getBundle("Zoo", myLocale);
+        System.out.println(bundle.getString("hello"));
+        System.out.println(bundle.getString("open"));
+    }
+}
