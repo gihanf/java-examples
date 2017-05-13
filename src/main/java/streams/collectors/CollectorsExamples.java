d40fb96cd65741f1f7a1ad3178fc330eb45b0266
gihanf <gihan.fernando@gmail.com>
2017-05-13 22:35:40 +1000

    Comments for stream examples.

--- src/main/java/streams/collectors/CollectorsExamples.java
+++ src/main/java/streams/collectors/CollectorsExamples.java
@@ -10,9 +10,12 @@ public class CollectorsExamples {
     public static void main(String[] args) {
         IntStream numbers = IntStream.of(1, 2, 3, 4);
         Stream<Integer> boxedIntegers = numbers.boxed();
+
+        /** partitionedBy is a collectors method: Collectors.partitionedBy **/
         Map<Boolean, Set<Integer>> partitionedByEvenOdd = boxedIntegers.collect(Collectors.partitioningBy(i -> i % 2 == 0, Collectors.toSet()));
         System.out.println("partitioned = " + partitionedByEvenOdd);
 
+        /** groupingBy is a collectors method: Collectors.groupingBy **/
         Stream<String> stringStream = Stream.of("Peter", "Mahendar", "Robbie", "StinkyPete", "gihan", "Daniel", "melcar");
         Map<Integer, List<String>> grouped = stringStream.collect(Collectors.groupingBy(String::length));
         System.out.println("grouped = " + grouped);
