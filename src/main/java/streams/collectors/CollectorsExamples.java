b826bb031c6a775583d6eeae10eaf5f49c832a0e
gihanf <gihan.fernando@gmail.com>
2017-05-13 22:34:04 +1000

    Added some stream examples

---
+++ src/main/java/streams/collectors/CollectorsExamples.java
@@ -0,0 +1,20 @@
+package streams.collectors;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class CollectorsExamples {
+
+    public static void main(String[] args) {
+        IntStream numbers = IntStream.of(1, 2, 3, 4);
+        Stream<Integer> boxedIntegers = numbers.boxed();
+        Map<Boolean, Set<Integer>> partitionedByEvenOdd = boxedIntegers.collect(Collectors.partitioningBy(i -> i % 2 == 0, Collectors.toSet()));
+        System.out.println("partitioned = " + partitionedByEvenOdd);
+
+        Stream<String> stringStream = Stream.of("Peter", "Mahendar", "Robbie", "StinkyPete", "gihan", "Daniel", "melcar");
+        Map<Integer, List<String>> grouped = stringStream.collect(Collectors.groupingBy(String::length));
+        System.out.println("grouped = " + grouped);
+    }
+}
--- src/main/java/streams/sources/StreamSource.java
+++ src/main/java/streams/sources/StreamSource.java
@@ -1,8 +1,10 @@
 package streams.sources;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Map;
+import java.util.stream.*;
 
 public class StreamSource {
 
@@ -19,5 +21,30 @@ public class StreamSource {
 
     /** Also creates an infinite stream **/
     Stream<Integer> oddNumbers = Stream.iterate(1, n -> n + 2);
+
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.iterate("", (s) -> s + "1");
+
+        System.out.println(Stream.iterate(1, x -> x++)
+                .limit(5)
+                .map(x -> "" + x)
+                .collect(Collectors.joining()));
+
+        Stream<Integer> iterate = Stream.iterate(1, x -> x * x);
+
+        IntStream blah = IntStream.generate(() -> 1);
+        IntStream blah2 = IntStream.of(1, 2, 3, 4);
+
+        Stream<Integer> boxedIntegers = blah2.boxed();
+        Map<Boolean, List<Integer>> results = boxedIntegers.collect(Collectors.partitioningBy(i -> i % 2 == 0));
+        System.out.println("results = " + results);
+
+        Stream<String> words = Stream.of("ab", "asd", "g");
+
+        IntStream iStream = IntStream.of(1, 2, 3, 4);
+        DoubleStream dstream = DoubleStream.of(1.2, 1.4, 1.6);
+        LongStream lstream = LongStream.of(1, 2, 3);
+    }
+
 
 }
